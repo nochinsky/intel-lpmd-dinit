08412cdde85613fd8b820d1c82dfb4fdb00bc19b
nochinsky <nochinsky@users.noreply.github.com>
2026-06-01 22:52:21 +0300

    Final cleanup: remove Intel CI, fix config comments, strip systemctl from test script

--- tests/lpm_test_interface.sh
+++ tests/lpm_test_interface.sh
@@ -1,40 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$(whoami)" != "root" ]
-then
+if [ "$(whoami)" != "root" ]; then
 	echo "This script must be run as root"
 	exit 1
 fi
 
 opt_no=
-if [ ! -z "$1" ]
-then
-        opt_no=$1
+if [ ! -z "$1" ]; then
+	opt_no=$1
 fi
 
-while true;
-do
-	if [ -z "$1" ]
-	then
-		echo "****options****"
-		echo "0 : Allow All CPUs"
+while true; do
+	if [ -z "$1" ]; then
+		echo "**** options ****"
 		echo "1 : Terminate"
 		echo "2 : LPM force on"
 		echo "3 : LPM force off"
 		echo "4 : LPM auto"
 		echo "5 : Quit"
-	        echo -n " Enter choice: "
-	        read opt_no
+		echo -n " Enter choice: "
+		read opt_no
 	fi
 
 	case $opt_no in
-	0)
-		echo "0 : Allow All CPUs"
-		echo -n " Enter Maximum CPU number"
-		read max_cpu
-		sudo systemctl set-property --runtime user.slice AllowedCPUs=0-$max_cpu
-		sudo systemctl set-property --runtime system.slice AllowedCPUs=0-$max_cpu
-		;;
 	1)
 		echo "1 : Terminate"
 		dbus-send --system --dest=org.freedesktop.intel_lpmd --print-reply /org/freedesktop/intel_lpmd org.freedesktop.intel_lpmd.Terminate
@@ -55,7 +43,6 @@ do
 		exit 0
 		;;
 	*)
-		echo "5 : Quit"
 		echo "Invalid option"
 	esac
 	[ ! -z "$1" ] && break
